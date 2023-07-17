164089b7b2fb0d39128850bb7ab4519937ca8e7f
Ivana Pusic <pusicka97@gmail.com>
2023-07-17 09:49:06 +0200

    setup component functionality

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { useGlobalContext } from '../hooks/useGlobalContext';
 
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const { query, handleSearch } = useGlobalContext();
+  return (
+    <form className='search-form' onSubmit={(e) => e.preventDefault()}>
+      <h2>search hacker news</h2>
+      <input
+        type='text'
+        className='form-input'
+        value={query}
+        onChange={(e) => handleSearch(e.target.value)}
+      />
+    </form>
+  );
 };
 
 export default SearchForm;
